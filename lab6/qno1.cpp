b1aa7ac4a41b60e873ca82dc7c4f41538f0fcde4
d33panpaudel07 <141331201+d33panpaudel07@users.noreply.github.com>
2023-10-06 19:30:38 +0545

    Add files via upload

---
+++ lab6/qno1.cpp
@@ -0,0 +1,50 @@
+/*Qno1:Write a program to represent graph and test it properties. Take the graphs in
+the following figure as a sample.*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int vertices;
+	cout<<"Enter number of vertices : ";
+	cin>>vertices;
+	int mat[vertices][vertices];
+	int i,j;
+
+
+	for(char x='A';x<'A'+vertices;x++){
+		j=0;
+		for(char y='A';y<'A'+vertices;y++){
+			char uin=0;
+			cout<<"Is "<<x<<" connected to "<<y<<"? (y,n) : ";
+			cin>>uin;
+			if(uin == 'y' || uin == 'Y'){
+				mat[i][j]=1;
+				j++;
+			}
+			else{
+				mat[i][j]=0;
+				j++;
+			}
+		}
+		i++;
+	}
+	for(int i=0;i<vertices;i++){
+		char point1=65+i;
+		cout<<" "<<point1;
+		if(i==vertices-1){
+			cout<<endl;
+		}
+	}
+	for(int i=0;i<vertices;i++){
+		char point2=65+i;
+		cout<<point2<<" ";
+		
+		for(int j=0;j<vertices;j++){
+			cout<<mat[i][j]<<" ";
+			if(j==vertices-1){
+				cout<<endl;
+			}
+		}
+	}
+}
